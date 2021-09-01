572cfab3df5790cb0d98dac389a39526603f6a3e
DeepSkyWonder <87848500+DeepSkyWonder@users.noreply.github.com>
2021-09-01 19:01:30 +1200

    fix large stars

--- rebound/plotting.py
+++ rebound/plotting.py
@@ -348,7 +348,7 @@ def OrbitPlotAddFancyStars(ax,lw,slices=1.):
     starsurfacedensity = 0.8
 
     area = np.sqrt(np.sum(np.square(ax.transAxes.transform([1.,1.]) - ax.transAxes.transform([0.,0.]))))*slices
-    area = xlim**2
+    area = 3.24
     nstars = int(starsurfacedensity*area)
 
     #small stars
