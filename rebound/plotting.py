31a81f3421ba2462cdebb1de75e4cdf3c947fe6d
DeepSkyWonder <87848500+DeepSkyWonder@users.noreply.github.com>
2021-09-01 18:32:51 +1200

    fix large stars

--- rebound/plotting.py
+++ rebound/plotting.py
@@ -361,7 +361,7 @@ def OrbitPlotAddFancyStars(ax,lw,slices=1.):
     np.random.seed(1)
     
     #large stars
-    xy = np.random.uniform(size=(nstars//4,2))
+    xy = np.random.uniform(size=(int(nstars//4),2))
     ax.scatter(xy[:,0],xy[:,1], transform=ax.transAxes, alpha=0.1, s=15*lw, facecolor=starcolor, edgecolor=None, zorder=3)
     ax.scatter(xy[:,0],xy[:,1], transform=ax.transAxes, alpha=0.1, s=5*lw, facecolor=starcolor, edgecolor=None, zorder=3)
     ax.scatter(xy[:,0],xy[:,1], transform=ax.transAxes, alpha=0.5, s=2*lw, facecolor=starcolor, edgecolor=None, zorder=3)
